8ff614bf9fb35ce94cc7fae3b4b359c0ddced0c2
kakarlavenu <137424408+nubiiojm@users.noreply.github.com>
2023-07-23 16:52:02 +0530

    Update _variables.tf

--- _variables.tf
+++ _variables.tf
@@ -1,77 +1,36 @@
-variable "aws-region" {
-  description = "The AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "instance-ami" {
-  description = "The AMI (Amazon Machine Image) that identifies the instance"
-  type        = string
-  default     = "ami-053b0d53c279acc90"
-}
-
-variable "instance-type" {
-  description = "The instance type to be used"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "instance-key-name" {
-  description = "The name of the SSH key to associate to the instance. Note that the key must exist already."
+variable "aws_access_key" {
+  description = "AWS access key"
   type        = string
   default     = ""
 }
 
-variable "instance-associate-public-ip" {
-  description = "Defines if the EC2 instance has a public IP address."
-  type        = string
-  default     = "true"
-}
-
-variable "user-data-script" {
-  description = "The filepath to the user-data script, that is executed upon spinning up the instance"
+variable "aws_secret_key" {
+  description = "AWS secret key"
   type        = string
   default     = ""
 }
 
-variable "instance-tag-name" {
-  description = "instance-tag-name"
+variable "aws_region" {
+  description = "AWS region"
   type        = string
-  default     = "EC2-instance-created-with-terraform"
+  default     = "ap-south-1"
 }
 
-variable "vpc-cidr-block" {
-  description = "The CIDR block to associate to the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID"
+   default     = "ami-0f5ee92e2d63afc18"
 }
 
-variable "subnet-cidr-block" {
-  description = "The CIDR block to associate to the subnet"
-  type        = string
-  default     = "10.0.1.0/24"
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.small"
 }
 
-variable "vpc-tag-name" {
-  description = "The Name to apply to the VPC"
-  type        = string
-  default     = "VPC-created-with-terraform"
-}
-
-variable "ig-tag-name" {
-  description = "The name to apply to the Internet gateway tag"
-  type        = string
-  default     = "aws-ig-created-with-terraform"
-}
-
-variable "subnet-tag-name" {
-  description = "The Name to apply to the VPN"
-  type        = string
-  default     = "VPN-created-with-terraform"
-}
-
-variable "sg-tag-name" {
-  description = "The Name to apply to the security group"
-  type        = string
-  default     = "SG-created-with-terraform"
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "My EC2 Instance"
 }
